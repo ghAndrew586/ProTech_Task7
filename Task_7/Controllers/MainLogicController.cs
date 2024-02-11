2a8e38181e721fd1ec42c74e0f2bcccb28dd9cca
Andrey Kochetov <and-usmail-586@yandex.ru>
2024-02-11 15:47:27 +0500

    Create important files

---
+++ Task_7/Controllers/MainLogicController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task_7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainLogicController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get(string inputLine, int sortOption)
+        {
+            return Ok(inputLine);
+        }
+    }
+}
